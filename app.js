18f0080c39c162e7700a11ccd3e2bede2482e46e
Kuldeep <c0dysharma01@gmail.com>
2023-01-23 00:29:21 +0530

    added time period changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hoursEl = document.getElementById("time-box-hours");
 const minuteesEl = document.getElementById("time-box-minutees");
 const secondsEl = document.getElementById("time-box-seconds");
 const ampmEl = document.getElementById("time-box-am-pm");
+const timePeriod = document.getElementById("time-period");
 
 const updateTime = () => {
   let time = new Date().toLocaleTimeString("en-US", {
@@ -16,6 +17,22 @@ const updateTime = () => {
   minuteesEl.innerText = time[1];
   secondsEl.innerText = time[2].split(" ")[0];
   ampmEl.innerText = time[2].split(" ")[1];
+
+  if (ampmEl.innerText.toLowerCase() === "pm") {
+    if (hoursEl.innerText === "12") timePeriod.innerText = "Noon";
+    else if (
+      parseInt(hoursEl.innerText) >= 1 &&
+      parseInt(hoursEl.innerText) < 4
+    )
+      timePeriod.innerText = "Afternoon";
+    else if (
+      parseInt(hoursEl.innerText) >= 4 &&
+      parseInt(hoursEl.innerText) < 8
+    )
+      timePeriod.innerText = "Evening";
+    else timePeriod.innerText = "Night";
+    return;
+  } else timePeriod.innerText = "Morning";
 };
 
 window.setInterval(() => updateTime(), 1000);
